6004a417e57ff722fe9de13d8a4dcfe378195480
peiyan531 <56054975+peiyan531@users.noreply.github.com>
2020-06-23 17:21:47 +0800

    Add files via upload

---
+++ eapp-corp-quick-start-java-master/src/main/java/com/config/Constant.java
@@ -0,0 +1,15 @@
+package com.config;
+
+/**
+ * 项目中的常量定义类
+ */
+public class Constant {
+    /**
+     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppKey
+     */
+    public static final String APP_KEY = "dingklyylsdgxxzpibfv";
+    /**
+     * 开发者后台->企业自建应用->选择您创建的E应用->查看->AppSecret
+     */
+    public static final String APP_SECRET="Mv3FSgwkT0szELCfWFW1naYs3zi_mE__kWAd7j9j-USONn3fOwb3WmK7aFrR9EGM";
+}
