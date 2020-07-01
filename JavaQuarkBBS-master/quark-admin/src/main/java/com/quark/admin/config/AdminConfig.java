2444705bed114317ce0a4641e4395cd947a01fb7
YKlijia <2391902630@qq.com>
2020-07-01 17:31:19 +0800

    添加

---
+++ JavaQuarkBBS-master/quark-admin/src/main/java/com/quark/admin/config/AdminConfig.java
@@ -0,0 +1,12 @@
+package com.quark.admin.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by py on 17-7-31.
+ */
+@Configuration
+@ComponentScan(basePackages = "com.quark.common")
+public class AdminConfig {
+}
